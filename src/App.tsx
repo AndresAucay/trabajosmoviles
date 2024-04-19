3ad700d4f1de95f9588dee1a66cdccb8d0ed4cfc
Andres <maaucay@sudamericano.edu.ec>
2024-04-19 00:27:04 -0500

    deber2

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,29 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+export enum ResistorValues {
+  black = 0,
+  brown = 1,
+  red = 2,
+  orange = 3,
+  yellow = 4,
+  green = 5,
+  blue = 6,
+  violet = 7,
+  grey = 8,
+  white = 9
 }
 
-export default App
+type Color = keyof typeof ResistorValues;
+
+export function decodedValue([first, second]: Color[]): number {
+  return Number(`${ResistorValues[first]}${ResistorValues[second]}`);
+}
+
+function App() {
+  const valorResistor = decodedValue(['red', 'red']);
+
+  console.log(`Valor del resistor decodificado: ${valorResistor}`);
+
+  return null;
+}
+
+export default App;
